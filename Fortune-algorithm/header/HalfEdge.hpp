01000ef8c78508eb7aeb9b0f5c5f36ff7b39dffc
Jhony Angulo <jhony.angulo@utec.edu.pe>
2019-11-29 07:48:31 -0500

    Added graphics

--- Fortune-algorithm/header/HalfEdge.hpp
+++ Fortune-algorithm/header/HalfEdge.hpp
@@ -14,5 +14,13 @@ struct HalfEdge {
   void print () {
     printf ("(%f,%f) -> (%f,%f)\n", from.x, from.y, to.x, to.y);
   }
+  void render () {
+    glColor3f(0.0,1.0,.0);
 
+    glBegin(GL_LINE_LOOP);
+		glVertex2f(from.x/10000, from.y/10000);
+		glVertex2f(to.x/10000, to.y/10000);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+  }
 };
--- Fortune-algorithm/src/main.cpp
+++ Fortune-algorithm/src/main.cpp
@@ -2,36 +2,86 @@
 #include "../header/VoronoiDiagram.hpp"
 #include "../header/Visualization.hpp"
 #include "../header/FortuneAlgorithm.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 using T = float;
 
+class Window {
+	GLFWwindow *window{nullptr};
+	std::string title;  
+  Arguments <T> arg;
+
+
+  FortuneAlgorithm <T> *fortune; 
+  VoronoiDiagram <T> diagram;
+
+public:
+	Window(int width, int height,int n, int p,std::string title,
+         int argc, char** argv
+         
+         )
+  {
+    util::parse_arguments (argc, argv, arg);
+    arg.verify ();
+    if (arg.sites.empty ()) {
+      util::generate_random_sites (arg.n_points, arg.min_val, arg.max_val, arg.sites);
+    }
+
+    fortune = new  FortuneAlgorithm <T> (arg.sites);
+    diagram = fortune->getDiagram ();
+
+    glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    window = glfwCreateWindow( width, height, title.c_str(), NULL, NULL);
+    glfwMakeContextCurrent( window );
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glLineWidth(5);
+  }
+
+	void display()
+  {
+    GLint width,height,old_width, old_height;
+    glfwGetWindowSize(window, &width, &height);
+
+    while ( !glfwWindowShouldClose(window) ) {
+      glfwGetWindowSize(window, &width, &height);
+
+      glLoadIdentity();
+      glViewport(0, 0, width, height);
+
+      glPointSize(3);
+      glClearColor(0, 0, 0, 1);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      glMatrixMode(GL_PROJECTION_MATRIX);
+      glLoadIdentity();
+    
+      //Renderizar
+      for (auto edge: diagram.edges) {
+        edge -> render();
+      }      
+    
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+  }
+};
+
+
+
+
 int main (int argc, char** argv) {
 #ifdef FORTUNE
-  Arguments <T> arg;
-  util::parse_arguments (argc, argv, arg);
-  arg.verify ();
-  if (arg.sites.empty ()) {
-    util::generate_random_sites (arg.n_points, arg.min_val, arg.max_val, arg.sites);
-  }
-  FortuneAlgorithm <T> fortune (arg.sites);
-  VoronoiDiagram <T> diagram = fortune.getDiagram ();
-
-  // Iterating cells
-  for (auto cell: diagram.cells) {
-    Point <T> site = cell.site;
-    HalfEdge <T>* edge = cell.edges;
-    do {
-      Point <T> from = edge->from;
-      Point <T> to = edge->to;
-      // do something with (site, from, to)
-      edge = edge->next;
-    } while (edge != cell.edges);
-  }
+	Window w(1000,1000, 10, 2,"Fortune",
+         argc,  argv);
+  w.display();
 
   // Iterating vertices
-  for (auto edge: diagram.edges) {
-    edge -> print ();
-  }
+
 
 #else
   std::cout << "Ingrese el número de cores:" << "\n";
