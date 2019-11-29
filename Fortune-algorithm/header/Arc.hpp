68bb48e647f6eb7754208fc590d1825de87d0b28
TISparta <jose.garcia@utec.edu.pe>
2019-11-29 00:18:25 -0500

    foo

--- Fortune-algorithm/header/Arc.hpp
+++ Fortune-algorithm/header/Arc.hpp
@@ -3,14 +3,15 @@
 #include "Point.hpp"
 
 template <typename T>
-struct event;
+class Event;
 
 template <typename T>
 struct Arc {
   Point <T> p;
-  Arc *prev = 0;
-  Arc *next = 0;
-  
+  Arc <T>* prev = 0;
+  Arc <T>* next = 0;
+  Event <T> *event;
+
   Arc (Point <T> p, Arc *prev = 0, Arc *next = 0):
     p(p), prev(prev), next(next) {}
 };
